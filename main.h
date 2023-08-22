43f042ae92ce1ed5906884840e48b5753ffd529a
oladapsy <dapoodedeyi@yahoo.com>
2023-08-22 13:25:31 -0700

    The header file containing all prototypes

--- main.h
+++ main.h
@@ -1,7 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
 int printchar(char achar);
 int printstring(char *string);
+int _printf(const char *format, ...);
+int printint(int num);
 
 #endif
